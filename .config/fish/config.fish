9bdf5b7d2cf51f198a7f32888220a0a0a5e41223
Victor Parchment <services@vparchment.com>
2025-12-20 19:14:00 +0000

    feat: launch hyprland on boot

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,13 @@ if test "$TERM" = xterm-ghostty
     set -gx TERM xterm-256color
 end
 
+# Hyprland start on boot
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec Hyprland
+    end
+end
+
 set fish_greeting
 
 # Hydro configuration options
